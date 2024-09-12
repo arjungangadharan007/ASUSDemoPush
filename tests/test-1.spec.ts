fba616714acbbb6428b26a91312e85dcda0ac89b
Arjun <arjungangadharan007@gmail.com>
2024-09-12 21:06:06 +0530

    2nd try

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { Console } from 'console';
 
 test.only('test', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
@@ -7,4 +8,6 @@ test.only('test', async ({ page }) => {
   await page.locator('[data-test="password"]').click();
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
+  console.log("message");
+  
 });
